a35e4c969bdc0681c0528b4b13b757f135fa13ab
Vyacheslav Goryachin <gor.vya4eslav@yandex.ru>
2020-08-20 00:52:30 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WeatherForecastOfCites from './components/weather-forecast-of-cities';
+
+const App = () => {
+  return (
+    <div className="App">
+      <WeatherForecastOfCites />
+    </div>
+  );
+}
+
+export default App;
